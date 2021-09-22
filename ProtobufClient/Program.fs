7436e0da9cb1d0d51c92ce609be376a7d82887d4
Brian Berns <brianberns@gmail.com>
2021-09-22 15:54:26 -0400

    Removed unneeded  task.

--- ProtobufClient/Program.fs
+++ ProtobufClient/Program.fs
@@ -9,22 +9,18 @@ open ProtobufCommon
 let calc () =
     task {
         use http = GrpcChannel.ForAddress("http://localhost:10042")
-
         let client = http.CreateGrpcService<ICalculator>()
         let! result = client.MultiplyAsync { X = 12; Y = 4 }
         printfn "%i" result.Result
     } |> fun t -> t.Result
 
 let clock () =
-    task {
-        use http = GrpcChannel.ForAddress("http://localhost:10042")
-
-        let client = http.CreateGrpcService<ITimeService>()
-        async {
-            for (min, sec) in client.SubscribeTupleAsync() |> AsyncSeq.ofAsyncEnum do
-                printfn "%s, %s" min sec
-        } |> Async.RunSynchronously
-    } |> fun t -> t.Result
+    use http = GrpcChannel.ForAddress("http://localhost:10042")
+    let client = http.CreateGrpcService<ITimeService>()
+    async {
+        for (min, sec) in client.SubscribeTupleAsync() |> AsyncSeq.ofAsyncEnum do
+            printfn "%s, %s" min sec
+    } |> Async.RunSynchronously
 
 [<EntryPoint>]
 let main _ =
