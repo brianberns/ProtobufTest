d517b6b810bdba9aa75f93d974a52175d5b2f07a
Brian Berns <brianberns@gmail.com>
2021-09-22 14:56:27 -0400

    Time service now working.

--- ProtobufClient/Program.fs
+++ ProtobufClient/Program.fs
@@ -1,16 +1,34 @@
 ﻿open ProtoBuf.Grpc.Client
 open Grpc.Net.Client
+
+open FSharp.Control
 open FSharp.Control.Tasks
 
 open ProtobufCommon
 
-[<EntryPoint>]
-let main _ =
-    GrpcClientFactory.AllowUnencryptedHttp2 <- true
+let calc () =
     task {
         use http = GrpcChannel.ForAddress("http://localhost:10042")
+
         let client = http.CreateGrpcService<ICalculator>()
         let! result = client.MultiplyAsync { X = 12; Y = 4 }
         printfn "%i" result.Result
-        return 0
     } |> fun t -> t.Result
+
+let clock () =
+    task {
+        use http = GrpcChannel.ForAddress("http://localhost:10042")
+
+        let client = http.CreateGrpcService<ITimeService>()
+        async {
+            for result in client.SubscribeAsync() |> AsyncSeq.ofAsyncEnum do
+                printfn "%A" result.Time
+        } |> Async.RunSynchronously
+    } |> fun t -> t.Result
+
+[<EntryPoint>]
+let main _ =
+    GrpcClientFactory.AllowUnencryptedHttp2 <- true
+    calc ()
+    clock ()
+    0
