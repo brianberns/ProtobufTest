c52b4dddac40159a6c2ca81fad418ab751fd3ac4
Brian Berns <brianberns@gmail.com>
2021-09-22 14:14:55 -0400

    More work on server.

--- ProtobufServer/Contract.fs
+++ ProtobufServer/Contract.fs
@@ -1,8 +1,22 @@
 ﻿namespace ProtobufServer
 
+open System
+open System.Threading.Tasks
+
 open ProtobufCommon
+
+open FSharp.Control
+
+type CalculatorService =
+    interface ICalculator with
+        member __.MultiplyAsync(req : MultiplyRequest) =
+            ValueTask<MultiplyResult>({ Result = req.X * req.Y })
 
 type TimeService() =
     interface ITimeService with
-        member this.SubscribeAsync() =
-            raise (System.NotImplementedException())
+        member __.SubscribeAsync() =
+            asyncSeq {
+                while true do
+                    yield { Time = DateTime.Now }
+                    do! Async.Sleep 1000
+            } |> AsyncSeq.toAsyncEnum
